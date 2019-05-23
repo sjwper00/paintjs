c7b48daad38e7a6957514a53e398edb0befa3083
jinwons94 <jinwons94@naver.com>
2019-05-23 18:21:35 +0900

    Paintingjs JavaScript 5(filling mode effect)

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
+const INITIAL_COLOR = "#2c2c2c"
 
 canvas.width = 500;
 canvas.height = 550;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 5.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -34,6 +39,7 @@ function onMouseMove(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event){
@@ -41,6 +47,22 @@ function handleRangeChange(event){
     ctx.lineWidth = size;
 }
 
+function handleModeClick(event){
+   if(filling === true){
+       filling = false;
+       mode.innerText = "Fill"
+   }    else    {
+       filling = true;
+       mode.innerText = "Paint";
+   }
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    
+}
 /*
 1. 'onMouseUp'과 'onMouseLeave'의 값이 같으므로 겹치는
 값은 객체지향으로 따로 함수로 설정한 후, 값을 함수
@@ -58,6 +80,7 @@ if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
 if(colors){
@@ -67,4 +90,8 @@ if(colors){
 
 if(range){
     range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
 }
